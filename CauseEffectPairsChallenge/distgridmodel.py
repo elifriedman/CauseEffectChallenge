178c660ba0843388b9d44375156db7779be36155
elifriedman <friedm3@cooper.edu>
2015-12-17 01:15:12 -0500

    distribution features

---
+++ CauseEffectPairsChallenge/distgridmodel.py
@@ -0,0 +1,44 @@
+import data_io
+import numpy as np
+from sklearn.neighbors.kde import KernelDensity
+
+try:
+  data
+except:
+  data = data_io.read_train_pairs()
+  vals = data.values
+  targ = data_io.read_train_target()
+  info = data_io.read_train_info()
+
+def normalizedata():
+  for i in range(len(vals)):
+    m = np.array([vals[i][0],vals[i][1]])
+    mn = m.min(1).reshape(2,1)
+    m = m-mn
+    mx = m.max(1).reshape(2,1)
+    m = m/mx
+    vals[i][0] = m[0,:]
+    vals[i][1] = m[1,:]
+  return vals
+
+def getgridcoords(N):
+  i = np.linspace(0,1,N)
+  x,y = np.meshgrid(i,i)
+  x=x.reshape((N**2))
+  y=y.reshape((N**2))
+  return np.array([x,y])
+
+N=5
+def createfeatmat(N):
+  grid = getgridcoords(N).T
+  featmat = np.zeros((len(vals),N**2))
+  for i in range(len(vals)):
+    m = np.array([vals[i][0],vals[i][1]]).T
+    k = KernelDensity(bandwidth=0.5/(N-1),kernel='gaussian')
+    k.fit(m)
+    featmat[i,:] = k.score_samples(grid)
+  return featmat
+
+
+if __name__ == "__main__":
+  pass
